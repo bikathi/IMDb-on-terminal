c7a5504158ae67ea576f1c92f6e439ab14201c68
CharonTheV <bikathimartin@outlook.com>
2022-05-01 20:19:15 +0300

    Refined the response object for better JSON mapping.

--- src/main/java/npc/martin/imdbonterminal/MoviesResultObject.java
+++ src/main/java/npc/martin/imdbonterminal/MoviesResultObject.java
@@ -7,17 +7,16 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 //the JSON respose is a (self-named) MoviesResultObject
 public class MoviesResultObject {
     //this object has a collection of movie matches- which we call MovieMatchObject
-    protected List<MovieMatchObject> d = null;
+    private List<MovieMatches> d = null;
 
     //and these are the other instance variables of this class
-    private String q;
-    protected Integer v;
+    private String q; //your search querry
 
-    public String getQ() {
+    public String getSearchQuerry() {
         return q;
     }
 
-    public List<MovieMatchObject> getMovieMatchObject() {
+    public List<MovieMatches> getMovieMatches() {
         return d;
     }
 
@@ -35,31 +34,30 @@ public class MoviesResultObject {
 }
 
 //this is the class representing a single match result- a movie match
-class MovieMatchObject {
-    //It has an image object- which we refer to by the ImageObject class
-    ImageObject i;
+class MovieMatches {
+    //It has an image object- which we refer to by the MoviePoster class
+    private MoviePoster i; //the poster for the movie
 
-    //and these are the other instance variables that a single match 'may' have
-    private String id;
-    String l;
-    String q;
-    Integer rank;
-    String s;
+    //and these are other instance variables of interest that a single match 'may' have
+    private String l; //movie label (name) on IMDb servers
+    private Integer rank; //movie rank on IMDb
+    private String s; //movie starrings
 
-    //Inside a single match result is embedded a list of videos related to that match
-    //This list is therefore of the type VideoObject
-    List<VideoObject> v;
-
-    public String getIMovieMatchObject() {
-        return id;
-    }
-
-    public List<VideoObject> getV() {
-        return v;
-    }
-
-    public ImageObject getImageObject() {
+    public MoviePoster getMoviePoster() {
         return i;
+    }
+    
+    public String getMovieLabel() {
+        return l;
+    }
+    
+    public String getMovieStarrings() {
+        return s;
+    }
+    
+    
+    public Integer getIMDbMovieRank() {
+        return rank;
     }
     
     @Override
@@ -75,47 +73,13 @@ class MovieMatchObject {
     }
 }
 
-//this is the class representing the ImageObject embedded inside a single movie match
-class ImageObject {
-    //these are the variables making up one image
-    Integer height;
-    private String imageUrl;
-    Integer width;
+//this is the class representing the MoviePoster embedded inside a single movie match
+class MoviePoster {
+    //these are the variables making up the image
+    private String imageUrl; //URL to view the image
 
     public String getImageUrl() {
         return imageUrl;
-    }
-
-    @Override
-    public String toString() {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.enable(SerializationFeature.INDENT_OUTPUT);
-            return mapper.writeValueAsString(this);
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
-        return null;
-    }
-}
-
-//and this is the class representing the VideoObject embedded inside a single movie match object
-class VideoObject {
-    //the Video has it's associated images- say a thumbnail-like
-    //we get it's properties from the ImageObject class because they are the same
-    ImageObject i;
-
-    //and these are the other variables forming one VideoObject
-    String id;
-    private String l;
-    String s;
-
-    public ImageObject getImageObject() {
-        return i;
-    }
-
-    public String getL() {
-        return l;
     }
 
     @Override
