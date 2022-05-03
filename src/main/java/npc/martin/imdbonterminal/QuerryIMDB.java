909ff20495b6cd1eff3ea5a03f03f2cf13d7866b
CharonTheV <bikathimartin@outlook.com>
2022-05-03 11:39:31 +0300

    Added options to handle input for core utility functionality.

--- src/main/java/npc/martin/imdbonterminal/QuerryIMDB.java
+++ src/main/java/npc/martin/imdbonterminal/QuerryIMDB.java
@@ -1,13 +1,37 @@
 package npc.martin.imdbonterminal;
 
-/**
- * Hello world!
- *
- */
-public class QuerryIMDB 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Command;
+
+@Command(name = "", description = "Start the IMDB querry utility.", mixinStandardHelpOptions = true, version = "QuerryIMBb-1.0 BETA")
+public class QuerryIMDB implements Runnable {
+    @Option(names = { "-q", "--querry" }, paramLabel = "SEARCH QUERRY", description = "Give the movie/ show phrase to querry.")
+    protected String searchQuerry;
+    
+    @Option(names = { "-g", "--general" }, description = "Give a general overview of search querry results.")
+    protected Boolean general;
+    
+    @Option(names = { "-d", "--detailed" }, description = "Give a detailed overview of each querry result.")
+    protected Boolean detailed;
+    
+    @Option(names = { "-s", "--save" }, description = "Save your search querry and your results as JSON.")
+    protected Boolean save;
+    
+    
+    @Override
+    public void run() {
+        
+    }
+    
+    public static void main( String[] args ) {
+        //for testing
+        new CommandLine(new QuerryIMDB()).execute("-h");
+        
+        
+        //for deployment
+//        int exitCode = new CommandLine(new QuerryIMDB()).execute(args);
+//        System.exit(exitCode);
     }
 }
