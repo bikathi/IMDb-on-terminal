197a2faced0c3d763411e05e7cc5cea14ab949d4
CharonTheV <bikathimartin@outlook.com>
2022-05-09 23:39:39 +0300

    Implemented logic that downloads available movie posters.

--- src/main/java/npc/martin/imdbonterminal/views/GenerateTables.java
+++ src/main/java/npc/martin/imdbonterminal/views/GenerateTables.java
@@ -1,9 +1,16 @@
 package npc.martin.imdbonterminal.views;
 
-import npc.martin.imdbonterminal.QuerryIMDB;
 import com.github.freva.asciitable.AsciiTable;
 import com.github.freva.asciitable.Column;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class GenerateTables {
     protected Character[] borderStyle = AsciiTable.FANCY_ASCII;
@@ -19,7 +26,7 @@ public class GenerateTables {
         System.out.println("Number of related results: " + numberOfSuccessfullMatches);
         
         //general overview of results
-        System.out.println("General results overview: ");
+        System.out.println("Results overview: ");
         
         System.out.println(AsciiTable.getTable(borderStyle, result.getMovieMatches(), Arrays.asList(
                 new Column().header("Movie Name").with(movie -> movie.getMovieLabel()),
@@ -28,5 +35,69 @@ public class GenerateTables {
                 new Column().header("Movie Poster").with(movie -> movie.getMoviePoster().getImageUrl()),
                 new Column().header("Year of Release").with(movie -> Integer.toString(movie.getYearOfRelease()))
         )));
+        
+        List<MovieMatches> movieMatches = result.getMovieMatches();
+        this.downloadMoviePosters(movieMatches);
+  
+    }
+    
+    public void downloadMoviePosters(List<MovieMatches> sourceList) {
+        Iterator it = sourceList.iterator();
+        
+        Integer numberOfPosters = sourceList.size();
+        
+        String hostOS = System.getProperty("os.name");
+        String userHome = System.getProperty("user.home");
+        
+        //first make the folder to store the posters in
+        this.makeDownloadsFolder(hostOS, userHome);
+        
+        //then download the posters
+        while(it.hasNext()) {
+            System.out.println("Processing result: " + numberOfPosters + " ...");
+            MovieMatches m = (MovieMatches) it.next();
+            
+            //get the image URL
+            String imageUrl = m.getMoviePoster().getImageUrl();
+            //create a file name
+            String fileName = m.getMovieLabel() + " poster";
+                
+            try {
+                System.out.println("Downloading poster: " + numberOfPosters + " ...");
+                numberOfPosters--;
+                
+                this.getImageFromNet(imageUrl, fileName);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } 
+        }
+    }
+    
+    public void makeDownloadsFolder(String os, String home) {
+        if(os.equals("Linux") || os.equals("MacOS") || os.equals("Windows")) {
+            //navigate to the downloads dir
+            File file = new File(home + File.separator + "Downloads" + File.separator + "Movie Posters");
+            
+            //make the Movie Posters folder there
+            file.mkdir();
+        } else {
+            System.out.println("The movie poster downloads feature is not supported on this OS.");
+        }
+    }
+    
+    public void getImageFromNet(String imageUrl, String fileName) throws IOException {
+        URL url = new URL(imageUrl);
+        InputStream is = url.openStream();
+        OutputStream os = new FileOutputStream(fileName);
+        
+        byte[] b = new byte[2040];
+        int length;
+        
+        while((length = is.read(b)) != -1) {
+            os.write(b, 0, length);
+        }
+        
+        is.close();
+        os.close();
     }
 }
