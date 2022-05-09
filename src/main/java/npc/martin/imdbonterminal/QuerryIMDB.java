b482b470d9cdbea156ba2bd747ea9e4d20da8b7d
CharonTheV <bikathimartin@outlook.com>
2022-05-09 11:47:52 +0300

    Update the entry class to support entry of multiple search querries.

--- src/main/java/npc/martin/imdbonterminal/QuerryIMDB.java
+++ src/main/java/npc/martin/imdbonterminal/QuerryIMDB.java
@@ -1,33 +1,36 @@
 package npc.martin.imdbonterminal;
 
-
+import java.util.ArrayList;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import npc.martin.imdbonterminal.models.*;
 
-@Command(name = "", description = "Start the IMDB querry utility.", mixinStandardHelpOptions = true, version = "QuerryIMBb-1.0 BETA")
+@Command(name = "", description = "Start the IMDB querry utility.", mixinStandardHelpOptions = true, 
+        version = "QuerryIMBb-1.0 BETA")
 public class QuerryIMDB implements Runnable {
-    @Option(names = { "-q", "--querry" }, paramLabel = "SEARCH QUERRY", description = "Give the movie/ show phrase to querry.")
+    @Option(names = { "-q", "--querry" }, paramLabel = "SEARCH QUERRY", 
+            description = "Provide the movie/ show phrase to querry.")
     protected String searchQuerry;
-    
-    @Option(names = { "-g", "--general" }, description = "Give a general overview of search querry results.")
-    protected Boolean general;
-    
-    @Option(names = { "-d", "--detailed" }, description = "Give a detailed overview of each querry result.")
-    protected Boolean detailed;
     
     @Option(names = { "-s", "--save" }, description = "Save your search querry and your results as JSON.")
     protected Boolean save;
     
+    @Option(names ={ "-m", "--multiple" }, description = "Use this flag to provide more than one search querries to the util.")
+    protected Boolean mult;
+    
+    @Option(names = {"-l", "--list"}, description = "List of querries you are searching for.", 
+            paramLabel = "QUERRY LIST", arity = "1...5")
+    protected ArrayList<String> searchQuerries = new ArrayList<String>(3);
     
     @Override
     public void run() {
-        
+        new ModelSingleRequest().makeRequest(searchQuerry);
     }
     
     public static void main( String[] args ) {
         //for testing
-        new CommandLine(new QuerryIMDB()).execute("-h");
+        new CommandLine(new QuerryIMDB()).execute("-q", "transformers", "-g");
         
         
         //for deployment
