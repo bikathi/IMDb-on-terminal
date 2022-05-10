8514c54f54513be5e700de71bffb79d4177e0cc3
CharonTheV <bikathimartin@outlook.com>
2022-05-10 11:35:51 +0300

    Removed my API Key.

--- src/main/java/npc/martin/imdbonterminal/models/ModelMultipleRequests.java
+++ src/main/java/npc/martin/imdbonterminal/models/ModelMultipleRequests.java
@@ -1,5 +1,11 @@
 package npc.martin.imdbonterminal.models;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import npc.martin.imdbonterminal.views.MoviesResultObject;
 import java.util.List;
 import kong.unirest.Unirest;
@@ -12,11 +18,36 @@ public class ModelMultipleRequests {
             String getRequestURL = "https://online-movie-database.p.rapidapi.com/auto-complete?q=" + querry;
             MoviesResultObject response = Unirest.get(getRequestURL)
                     .header("X-RapidAPI-Host", "online-movie-database.p.rapidapi.com")
-                    .header("X-RapidAPI-Key", "663ca5a111msh7c4791d9dff1181p132acdjsn58c849b9b499")
+                    .header("X-RapidAPI-Key", "YOUR RAPID API KEY")
                     .asObject(MoviesResultObject.class)
                     .getBody();
             
             new GenerateTables().generateGeneralTable(querry, response, getPosters);
+            
+            System.out.println("Initiating search querry save job... ");
+            this.saveToFile(response, querry);
+            System.out.println("Exiting search querry save job...");
+        }
+    }
+    
+    public void saveToFile(MoviesResultObject response, String filePrefix) {
+        String outputString = response.toString();
+        String fileNamePrefix = filePrefix;
+        
+        //get the user home
+        String home = System.getProperty("user.home");
+        
+        //make the storage directory in a hidden folder in /user/home
+        File file = new File(home + File.separator + ".IMDbforTerminal");
+        file.mkdir();
+        
+        //get path to the storage 
+        Path path = Paths.get(home + File.separator + ".IMDbforTerminal" + File.separator + fileNamePrefix + ".txt");
+        
+        try {
+            Files.writeString(path, outputString, StandardCharsets.UTF_8);
+        } catch(IOException ex) {
+            System.out.println("Invalid Path");
         }
     }
 }
--- src/main/java/npc/martin/imdbonterminal/models/ModelSingleRequest.java
+++ src/main/java/npc/martin/imdbonterminal/models/ModelSingleRequest.java
@@ -1,5 +1,11 @@
 package npc.martin.imdbonterminal.models;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 import npc.martin.imdbonterminal.views.MoviesResultObject;
 import kong.unirest.Unirest;
 import npc.martin.imdbonterminal.views.GenerateTables;
@@ -9,11 +15,36 @@ public class ModelSingleRequest {
         String getRequestURL = "https://online-movie-database.p.rapidapi.com/auto-complete?q=" + searchQuerry;
         MoviesResultObject response = Unirest.get(getRequestURL)
 	    .header("X-RapidAPI-Host", "online-movie-database.p.rapidapi.com")
-	    .header("X-RapidAPI-Key", "663ca5a111msh7c4791d9dff1181p132acdjsn58c849b9b499")
+	    .header("X-RapidAPI-Key", "YOUR RAPID API KEY")
 	    .asObject(MoviesResultObject.class)
             .getBody();
         
         
         new GenerateTables().generateGeneralTable(searchQuerry, response, getPosters);
+        
+        System.out.println("Initiating search querry save job... ");
+        this.saveToFile(response, searchQuerry);
+        System.out.println("Exiting search querry save job...");
+    }
+    
+    public void saveToFile(MoviesResultObject response, String filePrefix) {
+        String outputString = response.toString();
+        String fileNamePrefix = filePrefix;
+        
+        //get the user home
+        String home = System.getProperty("user.home");
+        
+        //make the storage directory in a hidden folder in /user/home
+        File file = new File(home + File.separator + ".IMDbforTerminal");
+        file.mkdir();
+        
+        //get path to the storage 
+        Path path = Paths.get(home + File.separator + ".IMDbforTerminal" + File.separator + fileNamePrefix + ".txt");
+        
+        try {
+            Files.writeString(path, outputString, StandardCharsets.UTF_8);
+        } catch(IOException ex) {
+            System.out.println("Invalid Path");
+        }
     }
 }
