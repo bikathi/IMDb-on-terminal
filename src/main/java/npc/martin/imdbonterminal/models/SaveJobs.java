0f5a99f6705ac4edad722e692992b3e0e861e525
CharonTheV <bikathimartin@outlook.com>
2022-05-13 13:43:40 +0300

    Removed unnecessary spaces from within code.

--- src/main/java/npc/martin/imdbonterminal/models/SaveJobs.java
+++ src/main/java/npc/martin/imdbonterminal/models/SaveJobs.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package npc.martin.imdbonterminal.models;
 
-/**
- *
- * @author bikathi_martin
- */
-public class SaveJobs {
-    
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import npc.martin.imdbonterminal.views.MoviesResultObject;
+
+public abstract class SaveJobs {
+    public void saveToFile(MoviesResultObject response, String filePrefix) {
+        String outputString = response.toString();
+        String fileNamePrefix = filePrefix;
+        
+        //get the user home
+        String home = System.getProperty("user.home");
+        
+        //make the storage directory in a hidden folder in /user/home
+        File file = new File(home + File.separator + ".IMDbforTerminal");
+        file.mkdir();
+        
+        //get path to the storage 
+        Path path = Paths.get(home + File.separator + ".IMDbforTerminal" + File.separator + fileNamePrefix + ".txt");
+        
+        try {
+            Files.writeString(path, outputString, StandardCharsets.UTF_8);
+        } catch(IOException ex) {
+            System.out.println("Invalid Path");
+        }
+    }
 }
